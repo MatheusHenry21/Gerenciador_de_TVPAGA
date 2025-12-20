4e79a5e5e8d5b34a5c40c4530abc9c78adb0f386
Matheus Henrique <teus.henrique021@gmail.com>
2025-12-20 18:14:14 -0300

    Gerenciador de clientes TV

---
+++ moldels/atualizarCliente.py
@@ -0,0 +1,17 @@
+
+from moldels.formatacao_dados import celular_novo_contato
+
+def dados_atualizar(nome, celular):
+    novo_nome = None
+    novo_celular = None
+
+    if nome is not None:
+        novo_nome = nome
+
+    if celular is not None:
+        novo_celular = celular_novo_contato(celular)
+
+    if novo_nome is None and novo_celular is None:
+        return None
+
+    return [novo_nome, novo_celular]
